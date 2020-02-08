0783e649242b33eabf155d8df79fab0474b84e32
Karol <karolg@o2.pl>
2020-02-08 13:16:30 +0100

    Class Car + Car Schema in Database

---
+++ src/main/java/pl/sda/projekt/pansamochodzik/Car.java
@@ -0,0 +1,20 @@
+package pl.sda.projekt.pansamochodzik;
+
+import org.aspectj.asm.IModelFilter;
+
+public class Car {
+
+    private int id;
+    private String marka;
+    private String model;
+    private double rocznik;
+    private int cena;
+
+    public Car() {
+        this.id = id;
+        this.marka = marka;
+        this.model = model;
+        this.rocznik = rocznik;
+        this.cena = cena;
+    }
+}
