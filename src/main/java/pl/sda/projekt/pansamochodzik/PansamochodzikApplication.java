ac2d711fd0925b48c20c16cea54158d9583edb9b
Karol <karolg@o2.pl>
2020-02-08 11:42:52 +0100

    Inital Commit

---
+++ src/main/java/pl/sda/projekt/pansamochodzik/PansamochodzikApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.projekt.pansamochodzik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PansamochodzikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PansamochodzikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/projekt/pansamochodzik/PansamochodzikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.projekt.pansamochodzik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PansamochodzikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
